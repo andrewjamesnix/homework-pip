d9282718abbe904691acfece5cce9f63efcc22f5
Andrew Nix <andrewnix@Andrews-MBP.lan>
2019-07-01 20:27:55 +0100

    More tests added

--- scripts/accordian.js
+++ scripts/accordian.js
@@ -1,16 +1,67 @@
     class AccordianComponent {
 
-    NUMBER_OF_ITEMS = 5;
+    NUMBER_OF_ITEMS = 2;
+    container = document.createElement('div');
+    nodeList = [];
+
+    init = () => {
+        // this.container = document.getElementById('container');
+
+        // if (document.getElementById('container') === null) {
+        //   container = document.createElement('div');
+        //   container.setAttribute('id', 'container');
+        // } else {
+        //   container = document.getElementById('container');
+        //   container.innerHTML = '';
+        // }
+        this.container.setAttribute('id', 'container');
+        document.body.appendChild(this.container);
+         // document.body.appendChild(container);
+        accordian.createDom();
+        accordian.addListeners();
+    };
+
+    createDom = () => {
+        let headingDiv, contentDiv;
+
+        for(let i = 0; i < this.NUMBER_OF_ITEMS; i++) {
+            headingDiv = this.createElement('heading', i);
+            contentDiv = this.createElement('content', i);
+            this.nodeList.push(headingDiv);
+            this.nodeList.push(contentDiv);
+        }
+        // container = document.createElement('div');
+        // container.setAttribute('id', 'container');
+
+        for(let i = 0; i < this.nodeList.length; i++) {
+            this.container.append(this.nodeList[i]);
+        }
+
+        this.mainEl =  this.container.querySelectorAll(".heading");
+        this.contentEl =  this.container.querySelectorAll(".content");
+    };
+
+    createElement = (name, i) => {
+        let newElement = document.createElement("div");
+        if (name === 'heading') {
+            newElement.setAttribute('class', 'main' + i + ' heading');
+            newElement.innerText =  (i + 1) + this.suffix(i + 1) + ' Heading ';
+        } else {
+            newElement.setAttribute('class', 'content' + i + ' content');
+            newElement.innerText =  (i + 1) + '. Lorem gypsum conut el sadat el plinky plonky ';
+        }
+        return newElement;
+    };
 
     addListeners = () => {
-        if (this.mainEl.length > 0) {
-            this.mainEl.forEach((header, i) => {
-                header.addEventListener('click', () => {
-                    this.removeActiveClass();
-                    this.contentEl[i].classList.toggle('active');
-                });
+      if (this.mainEl.length > 0) {
+        this.mainEl.forEach((header, i) => {
+            header.addEventListener('click', () => {
+                this.removeActiveClass();
+                this.contentEl[i].classList.toggle('active');
             });
-        }
+        });
+      }
     };
 
     removeActiveClass = () => {
@@ -28,56 +79,8 @@
                 (i === 3 && j!== 13 ) ? 'rd' :
                     'th';
     };
-    createElement = (name, i) => {
-        let newElement = document.createElement("div");
-        if (name === 'heading') {
-            newElement.setAttribute('class', 'main' + i + ' heading');
-            newElement.innerText =  (i + 1) + this.suffix(i + 1) + ' Heading ';
-        } else {
-            newElement.setAttribute('class', 'content' + i + ' content');
-            newElement.innerText =  (i + 1) + '. Lorem gypsum conut el sadat el plinky plonky ';
-        }
-        return newElement;
-    };
 
-    init = () => {
-        let container = document.getElementById('container');
-        if (document.getElementById('container') !== null) {
-            container = document.getElementById('container');
-            container.innerHTML = null;
-        } else {
-            let container = document.createElement('div');
-            container.setAttribute('id', 'container');
-        }
 
-        document.body.append(container);
-        accordian.createDom();
-        accordian.addListeners();
-    };
-
-    createDom = () => {
-        let headingDiv, contentDiv, listArray = [];
-
-        for(let i = 0; i < this.NUMBER_OF_ITEMS; i++) {
-            headingDiv = this.createElement('heading', i);
-            contentDiv = this.createElement('content', i);
-            listArray.push(headingDiv);
-            listArray.push(contentDiv);
-        }
-
-        for(let i = 0; i < listArray.length; i++) {
-            document.getElementById('container').append(listArray[i]);
-        }
-        this.container = document.getElementById('container');
-        this.mainEl =  this.container.querySelectorAll(".heading");
-        this.contentEl =  this.container.querySelectorAll(".content");
-    }
 }
 
 const accordian = new AccordianComponent();
-
-
-
-
-
-
--- scripts/accordian.spec.js
+++ scripts/accordian.spec.js
@@ -1,29 +1,65 @@
-describe('Accordian', () => {
+describe('Accordian suffix method', () => {
     const component = new AccordianComponent();
-    component.init();
-    beforeEach(() => {
-        component.init();
+    describe('suffix method', () => {
+        it('should return correct number suffix for 1', () => {
+            let suffix = component.suffix(1);
+            expect(suffix).toBe("st");
+        });
 
+        it('should return correct number suffix for 2', () => {
+            let suffix = component.suffix(2);
+            expect(suffix).toBe("nd");
+        });
 
+        it('should return correct number suffix for 3', () => {
+            let suffix = component.suffix(3);
+            expect(suffix).toBe("rd");
+        });
+
+        it('should return correct number suffix for 4', () => {
+            let suffix = component.suffix(4);
+            expect(suffix).toBe("th");
+        });
+
+        it('should return correct number suffix for 112', () => {
+            let suffix = component.suffix(112);
+            expect(suffix).toBe("th");
+        });
+    });
+    describe(' createElement method', () => {
+
+        it('should return a HTML Node with correct classNames', () => {
+            let node = component.createElement("heading", 2);
+            expect(node.className).toBe('main2 heading');
+        });
+
+        it('should return a HTML Node with correct classNames', () => {
+            let node = component.createElement("content", 7);
+            expect(node.className).toBe('content7 content');
+        });
+
+      });
+
+    describe(' createDom method', () => {
+        beforeAll(() => {
+          component.NUMBER_OF_ITEMS = 5;
+        });
+        it('should return a HTML Node with correct number of nodes', () => {
+            component.createDom();
+            expect(component.nodeList.length).toBe(10);
+            expect(component.mainEl.length).toBe(5);
+            expect(component.contentEl.length).toBe(5);
+        });
     });
 
-    // it('should render 8 divs', () => {
-    //     expect(component).toBe(8);
-    // });
-    //
-    // it('should render 4 heading elements', () => {
-    //     expect(component.headingEls.length).toBe(4);
-    // });
-    //
-    // it('should render 4 content elements', () => {
-    //     expect(component.contentEls .length).toBe(4);
-    // });
-    //
-    // it('should render 4 content elements', () => {
-    //     component.headingEls[0].click();
-    //     console.log(component.contentEls);
-    //     expect(component.contentEls).toBe('none');
-    // });
-
-
+  // describe(' addListeners method', () => {
+  //
+  //     it('should return a HTML Node with correct number of nodes', () => {
+  //         component.createDom();
+  //         clickRow = sinon.spy();
+  //         component.mainEl[0].clock('click')
+  //         expect(component.mainEl.length).toBe(4);
+  //         expect(component.contentEl.length).toBe(8);
+  //     });
+  // });
 });
