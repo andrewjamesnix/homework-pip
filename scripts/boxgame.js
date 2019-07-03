61dff20dbbf9e619db7fe3fd6224ed5e7f89fbc3
andrew.nix <andrew.nix@gamesys.co.uk>
2019-07-03 18:01:52 +0100

    Work In Progress

--- scripts/boxgame.js
+++ scripts/boxgame.js
@@ -1,95 +1,130 @@
 class ChaserGameComponent {
+    box = {};
+    score = document.createElement("div");
+    squares = [];
+    container = document.createElement('div');
+    gameAreaEl = document.createElement("div");
+    gamebox = {
+        x: Math.floor(8),
+        y: Math.floor(2)
+    };
+    player = {
+        speed: 100,
+        square: 1,
+        score: 0
+    };
 
-box = {};
-score = document.querySelector(".score");
-gameAreaEl = document.querySelector(".gameArea");
-gameArea = this.gameAreaEl.getBoundingClientRect();
-squares = [];
-gamebox = {
-    x: Math.floor(this.gameArea.width/100),
-    y: Math.floor(this.gameArea.height/100)
-};
 
-player = {
-    speed: 100,
-    square: 1,
-    score: 0
-};
-init = () => {
-  document.addEventListener('DOMContentLoaded', this.build);
-  document.addEventListener('keyup', function (e) {
-      const allowKey = {
-          37:'left',
-          38: 'up',
-          39: 'right',
-          40: 'down'
-      };
-      if(allowKey[e.keyCode])
-      {game.handleKey(allowKey[e.keyCode])}
-  });
-};
+    init = () => {
+        this.gameAreaEl.innerHTML = '';
+        this.container.innerHTML = '';
+        this.score.innerHTML = '';
+        this.player = {
+            speed: 100,
+            square: 1,
+            score: 0
+        };
+        let gameArea = this.setGameArea();
+        this.gamebox = {
+            x: Math.floor(gameArea.width/100),
+            y: Math.floor(gameArea.height/100)
+        };
+        this.box = {};
+        document.body.appendChild(this.container);
+        this.createGameAreaDom();
+        this.addListeners();
+        this.build();
+    };
 
-makeActive = () => {
-    let randomIndex = Math.floor(Math.random() * this.squares.length);
+    addListeners = () => {
+        document.addEventListener('DOMContentLoaded', this.build);
+        document.addEventListener('keyup', function (e) {
+            const allowKey = {
+                37:'left',
+                38: 'up',
+                39: 'right',
+                40: 'down'
+            };
+            if(allowKey[e.keyCode]) {
+                game.handleKey(allowKey[e.keyCode])
+            }
+        });
+    };
 
-    if (randomIndex != 0 && this.player.square != randomIndex){
-        this.squares[randomIndex].classList.add('active');
-    } else {
-        this.makeActive();
-    }
+    createGameAreaDom = () => {
+        this.gameAreaEl.setAttribute("class", "gameArea");
+        this.score.setAttribute("class", "score");
+        this.container.appendChild(this.gameAreaEl);
+        this.container.appendChild(this.score);
+        this.gameAreaEl.getBoundingClientRect();
+    };
 
-};
+    setGameArea() {
+        return this.gameAreaEl.getBoundingClientRect();
+    };
 
-handleKey = (key) => {
-    if (key === 'left' && this.box.x > this.gameArea.left) {
-        this.box.x -= this.player.speed;
-        this.player.square--;
-    }
-    if (key === 'right' && this.box.x < this.gameArea.right - this.box.offsetWidth) {
-        this.box.x += this.player.speed;
-        this.player.square++;
-    }
-    if (key === 'up'&& this.box.y > this.gameArea.top) {
-        this.box.y -= this.player.speed;
-        this.player.square -= this.gamebox.x;
-    }
-    if (key === 'down' && this.box.y < (this.gameArea.bottom - this.box.offsetHeight)) {
-        this.box.y += this.player.speed;
-        this.player.square += this.gamebox.x;
-    }
-    this.box.style.left = this.box.x + 'px';
-    this.box.style.top = this.box.y + 'px';
+    makeActive = () => {
+        let randomIndex = Math.floor(Math.random() * this.squares.length);
 
-    if (!!this.squares[this.player.square].classList.contains('active')){
-        this.squares[this.player.square].classList.remove('active')
-        this.makeActive();
-        this.player.score++;
-        this.score.innerHTML = this.player.score;
-    }
-}
-
-build = (key) => {
-    this.box = document.createElement("div");
-    this.box.classList.add("box");
-    this.box.x = this.gameArea.top;
-    this.box.y = this.gameArea.left;
-    this.box.style.top = this.box.y + 'px';
-    this.box.style.left = this.box.x + 'px';
-    this.gameAreaEl.appendChild(this.box);
-    let counter = 1;
-
-    for(let y = 0; y < this.gamebox.y; y++){
-        for(let x = 0; x < this.gamebox.x; x++){
-            this.squares[counter] = document.createElement("div");
-            this.squares[counter].innerHTML = counter;
-            this.squares[counter].classList.add('square');
-            this.gameAreaEl.appendChild(this.squares[counter]);
-            counter++;
+        if (randomIndex != 0 && this.player.square != randomIndex){
+            this.squares[randomIndex].classList.add('active');
+        } else {
+            this.makeActive();
         }
-     }
-    this.makeActive();
-}
+
+    };
+
+    handleKey = (key) => {
+        let gameArea = this.setGameArea();
+        if (key === 'left' && this.box.x > gameArea.left) {
+            this.box.x -= this.player.speed;
+            this.player.square--;
+        }
+        if (key === 'right' && this.box.x < gameArea.right - this.box.offsetWidth) {
+            this.box.x += this.player.speed;
+            this.player.square++;
+        }
+        if (key === 'up'&& this.box.y > gameArea.top) {
+            this.box.y -= this.player.speed;
+            this.player.square -= this.gamebox.x;
+        }
+        if (key === 'down' && this.box.y < (gameArea.bottom - this.box.offsetHeight)) {
+            this.box.y += this.player.speed;
+            this.player.square += this.gamebox.x;
+        }
+        this.box.style.left = this.box.x + 'px';
+        this.box.style.top = this.box.y + 'px';
+
+        if (!!this.squares[this.player.square].classList.contains('active')){
+            this.squares[this.player.square].classList.remove('active')
+            this.makeActive();
+            this.player.score++;
+            this.score.innerHTML = this.player.score;
+        }
+    };
+
+    build = (key) => {
+        let gameArea = this.setGameArea();
+        this.box = document.createElement("div");
+        this.box.classList.add("box");
+        this.box.x = gameArea.top;
+        this.box.y = gameArea.left;
+        this.box.style.top = this.box.y + 'px';
+        this.box.style.left = this.box.x + 'px';
+        this.gameAreaEl.appendChild(this.box);
+        let counter = 1;
+
+        for(let y = 0; y < this.gamebox.y; y++){
+          for(let x = 0; x < this.gamebox.x; x++){
+                this.squares[counter] = document.createElement("div");
+                this.squares[counter].innerHTML = counter;
+                this.squares[counter].classList.add('square');
+                this.gameAreaEl.appendChild(this.squares[counter]);
+                counter++;
+            }
+         }
+        this.makeActive();
+    };
 }
 const game = new ChaserGameComponent();
-
-game.init();
+//game.init();
--- scripts/boxgame.spec.js
+++ scripts/boxgame.spec.js
@@ -1,9 +1,20 @@
 describe('ChaserGameComponent', () => {
     const component = new ChaserGameComponent();
-    describe('handleKey method', () => {
-        it('should return correct number suffix for 1', () => {
-            let goLeft = component.handleKey("left");
-            expect(goLeft).toBe("stylesheet");
+    describe('init method', () => {
+        it('should create the DOM', () => {
+            component.init();
+            expect(document.querySelectorAll('#container').length).toBe(1);
+            expect(document.querySelectorAll('.gameArea').length).toBe(1);
+            expect(document.querySelectorAll('.score').length).toBe(1);
+        });
+    });
+    describe('build method', () => {
+        it('should create the DOM', () => {
+            component.build();
+            expect(document.querySelectorAll('.box').length).toBe(1);
+            expect(document.querySelectorAll('.square').length).toBe(16);
+            expect(document.querySelectorAll('.active').length).toBe(1);
+
         });
     });
 });
