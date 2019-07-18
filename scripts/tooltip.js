a5e8d3c97a13de31df4c88a52d3065fce05fad03
andrew.nix <andrew.nix@gamesys.co.uk>
2019-07-18 18:01:16 +0100

    more bits

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -1,21 +1,18 @@
 class TooltipComponent {
     tooltips = document.querySelectorAll('.tooltip');
     output = document.querySelector('.output');
-
+    myInterval = setInterval( () => {
+        this.output.style.display = 'none'
+    }, 3000);
     init = () => {
         this.tooltips.forEach(tooltip => {
             tooltip.addEventListener('mouseover', (e) => {
-                let myInterval;
                 let holder = e.srcElement.getAttribute('datatoolcontent');
-                clearInterval(myInterval);
-                console.log(e.clientX + ' x ' + e.clientY)
+                clearInterval(this.myInterval);
                 this.output.style.display = 'block';
                 this.output.style.top = e.clientY + 5 + "px";
                 this.output.style.left = e.clientX + 5 + "px";
                 this.output.innerHTML = holder;
-                myInterval = setInterval( () => {
-                    this.output.style.display = 'none'
-                }, 3000);
             });
             tooltip.addEventListener('mouseout', () => {
                 this.output.style.display = 'none';
